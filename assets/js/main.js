2ba7c685b0f7d66ef493f607bf4134eefaf4696a
Jeffersoncharlles <contato@jefferdeveloper.com>
2021-02-07 21:10:37 -0300

    pegando candidado commit

---
+++ assets/js/main.js
@@ -0,0 +1,90 @@
+
+//variaves de controle de acesso
+let seuVotoPara = document.querySelector('.d-1-1 span');
+let cargo = document.querySelector('.d-1-2 span');
+let numeros = document.querySelector('.d-1-3');
+let descricao = document.querySelector('.d-1-4');
+let lateral = document.querySelector('.d-1-right');
+let aviso = document.querySelector('.d-2');
+
+//variaves de ambiente
+let etapaAtual = 0;
+let numero = '';
+
+function comecarEtapa(){
+
+    let etapa = etapas[etapaAtual];
+
+    let numeroHtml = '';
+
+   for(let i=0;i<etapa.numeros ; i++){
+       if (i === 0) {
+        numeroHtml += '<div class="numero pisca"></div>';
+       }
+        numeroHtml += '<div class="numero"></div>';
+   }
+
+    seuVotoPara.style.display = 'none';
+    cargo.innerHTML = etapa.titulo;
+    descricao.innerHTML = '';
+    aviso.style.display = 'none';
+    lateral.innerHTML = '';
+    numeros.innerHTML = numeroHtml;
+}
+
+function atualizaInterface(){
+
+    let etapa = etapas[etapaAtual];
+    let candidato = etapa.candidatos.filter((item)=>{
+        if (item.numero === numero) {
+            return true;
+        }else{
+            return false;
+        }
+    });
+
+    if (candidato.length > 0) {
+        candidato = candidato[0];
+        seuVotoPara.style.display = 'block';
+        descricao.innerHTML = `Nome: ${candidato.nome}<br/>Partido: ${candidato.partido}`;
+
+        let fotosHtml = '';
+        for(let i in candidato.fotos){
+            fotosHtml+= `<div class="d-1-image"><img src="./assets/images/${candidato.fotos[i].url}" alt="">${candidato.fotos[i].legenda}</div>`;
+        }
+
+        lateral.innerHTML = fotosHtml;
+    }else{
+        
+    }
+
+}
+
+//funcoes 
+function clicou(n){
+    let elNumero = document.querySelector('.numero.pisca');
+    if (elNumero !== null) {
+        elNumero.innerHTML = n;
+        numero = `${numero}${n}`;
+
+        elNumero.classList.remove('pisca');
+        if ( elNumero.nextElementSibling !== null) {
+            elNumero.nextElementSibling.classList.add('pisca'); 
+            //pegar o proximo elemento e colocar class
+        }else{
+            atualizaInterface();
+        }   
+    }
+}
+
+function branco(){
+    
+}
+function corrige(){
+
+}
+function confirma(){
+
+}
+
+comecarEtapa();
